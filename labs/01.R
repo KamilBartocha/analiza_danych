780731ec2b60c5b01744acb60af582897ba2a54e
Kamil Bartocha <kamilbartocha53@gmail.com>
2022-03-09 16:50:30 +0100

    lab2 knn alg

--- labs/01.R
+++ labs/01.R
@@ -1,1 +1,7 @@
-# initial commit
+# k-nn model for wisc data
+wbcd <- read.csv("wisc_bc_data.csv", stringsAsFactors = FALSE)
+str(wbcd)
+# usuwamy id
+wbcd <- wbcd[-1]
+str(wbcd)
+table(wbcd$diagnosis)
