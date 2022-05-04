d147a53ddcbd120a1a894278f32fbc7e9e644556
Kamil Bartocha <kamilbartocha53@gmail.com>
2022-05-04 16:38:19 +0200

    drzewa regresyjne czesc 2

--- labs/drzewa_regresycje.R
+++ labs/drzewa_regresycje.R
@@ -1,5 +1,6 @@
 library(rpart)
 library(rpart.plot)
+library(Cubist)
 # TASK 1 
 cT <- c(1,1,1,2,2,3,4,5,5,6,6,7,7,7,7)
 c1<- c(1,1,1,2,2,3,4)
@@ -31,3 +32,27 @@ m.rpart
 summary(m.rpart)
 
 rpart.plot(m.rpart)
+
+p.rpart <- predict(m.rpart, wine_test)
+summary(p.rpart)
+summary(wine_test$quality)
+
+cor(p.rpart, wine_test$quality)
+
+MAE <- function(actual, predicted){
+  mean(abs(actual - predicted))
+}
+
+MAE(p.rpart, wine_test$quality)
+mean(wine_train$quality)
+
+MAE(5.87, wine_test$quality)
+m.cubist <- cubist(x = wine_train[-12], y=wine_train$quality)
+m.cubist
+summary(m.cubist)
+
+p.cubist <- predict(m.cubist, wine_test)
+summary(p.cubist)
+
+cor(p.cubist, wine_test$quality)
+MAE(wine_test$quality, p.cubist)
